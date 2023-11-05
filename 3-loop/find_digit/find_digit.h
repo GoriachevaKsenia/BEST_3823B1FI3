76a2be49c1e499b4545095e5a39a4644d7ea2d30
GoriachevaKsenia <Ksenia5Goriacheva.ru@yandex.ru>
2023-11-05 23:59:01 +0300

    Com

--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -1,8 +1,8 @@
 #include <math.h>
 
 char find_digit(double number, char digit){
-    /*long long num = round(number), n1,
-    int n, n2, d, d1;
+    long long num = round(number), n1, nu = num, nu1;
+    int n, n2, d, d1, l = 0, l1 = 0;
     int k = 0, k1 = 0;
     while (num > 0)
     {
@@ -11,12 +11,21 @@ char find_digit(double number, char digit){
         num /= 10;
         k +=1;
         }
-        else d = digit;
+        else {
+        d = digit;
+        break;
+        }
+    }
+    while (nu > 0)
+    {
+        nu /= 10;
+        l +=1;
     }
     if (d == digit)
-    return k;
+    return l - (k -1);
     else{
         n1 = number * 10000000000;
+        nu1 = n1;
         while (n1 > 0)
         {
         n2 = n1 % 10;
@@ -24,7 +33,16 @@ char find_digit(double number, char digit){
         n1 /= 10;
         k1 +=1;
         }
-        else d1 = digit;
-        }*/
-    return 100;
+        else {
+            d1 = digit;
+            break;
+        }
+        }
+        while (nu1 > 0)
+        {
+        n1 /= 10;
+        l1 +=1;
+        }
+    return l1 - (k1 - 1);
+    }
 }  
--- 3-loop/limit/limit.h
+++ 3-loop/limit/limit.h
@@ -1,7 +1,18 @@
 #include <math.h>
 
 double limit(double eps, int n_max){
-    /*double y_n = n / sqrt(pow(n, 2) + n);
-    if (abs(x - n_max)*/
-    return 0.0;
+   double x = 1;
+double c = 0;
+double y = sqrt(2) / 2;
+
+if (n_max == 1)
+return y;
+for (x = 1; x < n_max; x++)
+{
+c = y;
+y = (sqrt(x * x + x) * y + 1) / sqrt(x * x + 3 * x + 2);
+if ((y - c) < eps)
+break;
+}
+return y;
 }
