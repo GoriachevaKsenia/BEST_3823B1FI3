a6c2aebe1cd26b5f5b54798f5b55b9fd5bd47102
GoriachevaKsenia <Ksenia5Goriacheva.ru@yandex.ru>
2023-11-12 23:28:40 +0300

    Com

--- 4-array/little_big_converter/little_big_convert.h
+++ 4-array/little_big_converter/little_big_convert.h
@@ -8,17 +8,3 @@ unsigned int little_big_convert(unsigned int input){
     a4 = (input & 4278190080) / pow(2, 24);
     return a1 + a2 + a3 + a4;
 }
-
-/* long long array1[32];
-    for(int i = 0; i < 32; i++){
-        while (input >  0){
-        array1[i] = input % 2;
-        input /= 2;
-        }
-    }
-    long long array2[32];
-    for(int i = 31; i > 0; i--){
-        for(int j = 0; j < 32; j++){
-            array2[j] = array1[i];
-        }
-    }*/
