480ae7432bf02a03971877857073b98a82d36f55
GoriachevaKsenia <Ksenia5Goriacheva.ru@yandex.ru>
2023-11-12 21:25:04 +0300

    Com

--- 4-array/little_big_converter/little_big_convert.h
+++ 4-array/little_big_converter/little_big_convert.h
@@ -1,7 +1,11 @@
 #include <math.h>
 
 unsigned int little_big_convert(unsigned int input){
+    int k = 1, bin;
     int array[32];
+    while(input > 0){
+        bin = ; 
+    }
     for(int i = 0; i < 32; i++){
         while (input >  0){
         array[i] = input % 2;
--- 4-array/robot/robot_move.h
+++ 4-array/robot/robot_move.h
@@ -1,5 +1,16 @@
 #include <math.h>
 
 char robot_move(int xs[], int ys[], int num_points, int area_radius, int xt, int yt){
-    return 1;
+    for(int i = 0; i < num_points; i++){
+        for(int j = 0; j < num_points; j++){
+            if(i != j){
+                double k = (ys[i] - ys[j]) / (xs[i] - xs[j]);
+                double b = ys[i] - k * xs[i];
+                double d = pow((k * b - xt -yt * k), 2) - (k * k + 1) *(xt * xt + b * b - 2 * yt * b + yt * yt - area_radius * area_radius);
+                if (d >= 0)
+                return 1;
+            }
+        }
+    return 0;
+    }
 }
