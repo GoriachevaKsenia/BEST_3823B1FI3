58134a49fb5d4beeb75111c693e29d60756d65ad
GoriachevaKsenia <Ksenia5Goriacheva.ru@yandex.ru>
2023-11-12 19:45:16 +0300

    Com

--- 4-array/little_big_converter/little_big_convert.h
+++ 4-array/little_big_converter/little_big_convert.h
@@ -1,5 +1,12 @@
 #include <math.h>
 
 unsigned int little_big_convert(unsigned int input){
+    int array[32];
+    for(int i = 0; i < 32; i++){
+        while (input >  0){
+        array[i] = input % 2;
+        input /= 2;
+        }
+    }
     return 0;
 }
