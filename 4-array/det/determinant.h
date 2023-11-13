2196c0e9a92c4dbc466095b5e8f3e0dfc5780cd4
GoriachevaKsenia <Ksenia5Goriacheva.ru@yandex.ru>
2023-11-13 19:15:23 +0300

    Com

--- 4-array/det/determinant.h
+++ 4-array/det/determinant.h
@@ -1,5 +1,17 @@
 #include <math.h>
 
 int determinant(int matrix[], int size){
-    return 0;
+    int det;
+    if ((sqrt(size) > floor(sqrt(size))) & (sqrt(size) < (floor(sqrt(size)) + 1)))
+    return -404;
+    if (sqrt(size) == 1){
+        return matrix[0];
+    }
+    if (sqrt(size) == 2){
+        return matrix[0] * matrix[3] - matrix[1] * matrix[2];
+    }
+    if (sqrt(size) == 3){
+        return matrix[0] * matrix[4] * matrix[8] + matrix[2] * matrix[3] * matrix[7] + matrix[1] * matrix[5] * matrix[6] - matrix[2] * matrix[4] * matrix[6] - matrix[0] * matrix[5] * matrix[7] - matrix[1] * matrix[3] * matrix[8];
+    }
+    
 }
--- 4-array/find_hole/hole_finder.h
+++ 4-array/find_hole/hole_finder.h
@@ -1,5 +1,18 @@
 #include <math.h>
 
 int hole_finder(int matrix[], int length, int width){
-    return 0;
+    int size = length * width, min = 100000; 
+    double k;
+    for(int i = width + 1; i < size - (width + 1); i++){
+        if((matrix[i] < matrix[i - width]) & (matrix[i] < matrix[i + width]) & (matrix[i] < matrix[i - 1]) & (matrix[i] < matrix[i - 1])){
+            if(matrix[i] < min){
+                min = matrix[i];
+                k = i;
+            }
+        }
+    }
+    double i = ceil((k + 1) / (double) (width)) - 1;
+    double j = round((((k + 1) / (double) (width)) - floor((k + 1) / (double) (width))) * width) - 1;
+    int ind = i * 100 + j;
+    return ind;
 }
