7c47949ca0951a5d837bb2f126c807c70f7b2a96
GoriachevaKsenia <Ksenia5Goriacheva.ru@yandex.ru>
2023-11-19 15:05:13 +0300

    Com

--- 5-pointer/calc_size/calcsize.h
+++ 5-pointer/calc_size/calcsize.h
@@ -1,5 +1,7 @@
 #include <math.h>
 
 int calcsize(void* memory){
+    int a = *memory;
+    for(int i = 0; )
     return 0;
 }
--- 5-pointer/even/find_even.h
+++ 5-pointer/even/find_even.h
@@ -1,14 +1,14 @@
 #include <math.h>
+#include <stddef.h>
 
 int* find_even(int* arr, int size){
-    /*int k = 0;
+    int k = 0;
     for (int i = 0; i < size; i++){
         if (arr[i] % 2 == 0){
-            k = k;
-            break;
+            k = i;
+            return arr + k;
         }
-        else
-        k += 1;
-    }
-    return *(arr + k);*/
+        if ((i == size - 1) & (arr[i] % 2 != 0))
+        return NULL;
+    } 
 }
